bd270695e53187b56103b9486de075a10697a0af
Justin Mathieu <JMathieu@Silgancontainers.com>
2017-12-21 11:48:35 -0600

    Added config file

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
+const objection = require('objection');
+const Model = objection.Model;
+const Knex = require('knex');
+
+// Initialize knex connection. Make sure your .env file has correct keys.
+const knex = Knex({
+    client: 'mysql',
+    useNullAsDefault: true,
+    connection: {
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE
+    }
+});
 
 // Get our API routes
 const api = require('./server/routes/api');
@@ -13,7 +28,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Point static path to dist
+// Point static path to public
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set our api routes
@@ -21,7 +36,7 @@ app.use('/api', api);
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 /**
--- server/routes/api.js
+++ server/routes/api.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 /* GET api listing. */
 router.get('/', (req, res) => {
-    res.send('api works');
+    res.send('');
+});
+
+router.post('/login', (req, res) => {
+    console.log(req.body);
+    res.status(200).send(req.body);
 });
 
 module.exports = router;
