d61e8549823098630487b3dfe0bb34703412656b
Justin Mathieu <JMathieu@Silgancontainers.com>
2017-12-21 13:45:01 -0600

    Continued work on the login screen

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm, FormControl } from '@angular/forms';
 
 @Component({
     selector: 'app-login',
@@ -7,12 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+    @ViewChild(NgForm) form: NgForm;
+
     constructor() { }
 
     ngOnInit() {
     }
 
     public login(): void {
+
+        //Check validation first
+        if (this.form.invalid) {
+            return;
+        }
+
+        console.log('Valid form!');
+
     }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './account/login/login.component';
 import { RegisterComponent } from './account/register/register.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { LoginScreenComponent } from './account/login-screen/login-screen.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AuthService } from './services/auth.service';
+import { DashboardComponent } from './main/components/dashboard/dashboard.component';
+import { AuthGuard } from './auth.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginScreenComponent,
+    NavBarComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
